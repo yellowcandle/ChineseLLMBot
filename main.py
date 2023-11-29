12a73748eeff992b15d8b903a584d6db917180a6
Replit user <unknown>
2023-11-29 10:22:43 +0000

    Bug fix

--- main.py
+++ main.py
@@ -4,11 +4,22 @@ import feedparser
 from litellm import completion
 import random
 import tweepy
-import schedule
+# schedule is imported but never used; you might want to remove it if it's not needed later on
 
-# Assuming dotenv is needed, we can uncomment these lines
-# from dotenv import load_dotenv
-# load_dotenv()
+# Helper function to split text into tweet-sized segments
+def split_into_tweets(text, max_length=280):
+    words = text.split()
+    tweets = []
+    current_tweet = ''
+    for word in words:
+        if len(current_tweet) + len(word) + 1 > max_length:
+            tweets.append(current_tweet)
+            current_tweet = word
+        else:
+            current_tweet += (' ' + word) if current_tweet else word
+    if current_tweet:
+        tweets.append(current_tweet)
+    return tweets
 
 class TwitterClient:
     def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
@@ -19,40 +30,45 @@ class TwitterClient:
             access_token_secret=access_token_secret
         )
 
-    def tweet(self, text):
+    def tweet(self, text, in_reply_to_tweet_id=None):
+        # Checking if 'in_reply_to_tweet_id' is not None to decide on first tweet or reply
+        method = self.client.create_tweet if in_reply_to_tweet_id is None else self.client.reply
         try:
-            self.client.create_tweet(text=text)
+            response = method(text=text, in_reply_to_tweet_id=in_reply_to_tweet_id)
             print("Tweet successfully posted!")
+            return response
         except tweepy.TweepyException as e:
             print(f"An error occurred: {e}")
 
 def extract_text_without_html(summary):
+    # Using regular expression to remove HTML tags
     return re.sub('<[^<]+?>', '', summary)
 
 def generate_response(prompt, entry_text):
+    # Calling the OpenAI API to generate a response based on the text format provided by a random prompt
     response = completion(model="gpt-3.5-turbo",
                           messages=[{
-                              "content": prompt,
-                              "role": "system"
+                              "role": "system",
+                              "content": prompt
                           }, {
-                              "content": entry_text,
-                              "role": "user"
+                              "role": "user",
+                              "content": entry_text
                           }])
     return response['choices'][0]['message']['content']
 
 def select_random_prompt():
     prompts = [
-        "使用台灣作家瓊瑤的寫作風格重寫輸入的字句，必須在140字元以內!",
-        "使用作家金庸的寫作風格重寫輸入的字句，必須在140字元以內!",
-        "使用文言文重寫輸入的字句，必須在140字元以內!"
+        "使用台灣作家瓊瑤的寫作風格重寫輸入的字句",
+        "使用作家金庸的寫作風格重寫輸入的字句",
+        "使用文言文重寫輸入的字句"
     ]
     return random.choice(prompts)
 
 def main():
-    twitter_api_key = os.environ['TWITTER_API_KEY']
-    twitter_api_secret = os.environ['TWITTER_API_SECRET']
-    twitter_access_token = os.environ['TWITTER_ACCESS_TOKEN']
-    twitter_access_secret = os.environ['TWITTER_ACCESS_SECRET']
+    twitter_api_key = os.environ.get('TWITTER_API_KEY')
+    twitter_api_secret = os.environ.get('TWITTER_API_SECRET')
+    twitter_access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
+    twitter_access_secret = os.environ.get('TWITTER_ACCESS_SECRET')
 
     twitter_client = TwitterClient(twitter_api_key, twitter_api_secret, twitter_access_token, twitter_access_secret)
 
@@ -61,16 +77,30 @@ def main():
     rss_feed_url = 'https://www.info.gov.hk/gia/rss/general_zh.xml'
     feed = feedparser.parse(rss_feed_url)
 
+    response_text = ''
     if feed.entries:
         first_entry_title = extract_text_without_html(feed.entries[0].title)
 
-        openai_api_key = os.environ['OPENAI_API_KEY']
+        openai_api_key = os.environ.get('OPENAI_API_KEY')  # Ensure that the api key is used when calling the API
         response_text = generate_response(selected_prompt, first_entry_title)
 
-        if response_text:
-            tweet_text = first_entry_title + "\n\n" + response_text + "\n\n" + "#中文 #文學"
-            twitter_client.tweet(tweet_text)
-            print(tweet_text)
+    if response_text:
+        suffix = "#中文 #文學"
+        tweet_text = f"{first_entry_title}\n\n{response_text}"
+        tweets = split_into_tweets(tweet_text, max_length=180 - len(suffix) - 1)
+
+        conversation_id = None
+        for tweet in tweets:
+            full_tweet = f"{tweet}\n\n{suffix}" if tweet == tweets[-1] else tweet
+            print(f"{len(full_tweet)} chars")
+
+            if conversation_id is None:
+                response = twitter_client.tweet(full_tweet)
+                conversation_id = response.data['id'] if response else None
+            else:
+                twitter_client.tweet(full_tweet, in_reply_to_tweet_id=conversation_id)
+
+            print(full_tweet)
 
 if __name__ == "__main__":
-  main()
+    main()
