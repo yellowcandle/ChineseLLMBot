65a3ef45b78f8922673dc27d2322451a696f193b
Replit user <>
2023-11-29 09:34:48 +0000

    refactored code.

--- main.py
+++ main.py
@@ -1,70 +1,76 @@
 import os
 import re
 import feedparser
-import tweepy
 from litellm import completion
 import random
+import tweepy
+import schedule
 
-# Move dotenv import here if needed
+# Assuming dotenv is needed, we can uncomment these lines
 # from dotenv import load_dotenv
 # load_dotenv()
 
+class TwitterClient:
+    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
+        self.client = tweepy.Client(
+            consumer_key=consumer_key,
+            consumer_secret=consumer_secret,
+            access_token=access_token,
+            access_token_secret=access_token_secret
+        )
 
-def tweet_content(summary_text):
-  # Authenticate to Twitter
-  client = tweepy.Client(
-      consumer_key=os.environ['TWITTER_API_KEY'],
-      consumer_secret=os.environ['TWITTER_API_SECRET'],
-      access_token=os.environ['TWITTER_ACCESS_TOKEN'],
-      access_token_secret=os.environ['TWITTER_ACCESS_SECRET']
-  )
-  # Post a tweet
-  try:
-      client.create_tweet(text=summary_text)
-      print("Tweet successfully posted!")
-  except tweepy.TweepyException as e:
-      print(f"An error occurred: {e}")
+    def tweet(self, text):
+        try:
+            self.client.create_tweet(text=text)
+            print("Tweet successfully posted!")
+        except tweepy.TweepyException as e:
+            print(f"An error occurred: {e}")
 
+def extract_text_without_html(summary):
+    return re.sub('<[^<]+?>', '', summary)
 
-def response_output(response):
-  # Retrieve the actual message content from the response
-  content = response['choices'][0]['message']['content']
-  return content  # Return the content so it can be used elsewhere
+def generate_response(prompt, entry_text):
+    response = completion(model="gpt-3.5-turbo",
+                          messages=[{
+                              "content": prompt,
+                              "role": "system"
+                          }, {
+                              "content": entry_text,
+                              "role": "user"
+                          }])
+    return response['choices'][0]['message']['content']
 
-def randomize_prompt():
+def select_random_prompt():
+    prompts = [
+        "使用台灣作家瓊瑤的寫作風格重寫輸入的字句，必須在140字元以內!",
+        "使用作家金庸的寫作風格重寫輸入的字句，必須在140字元以內!",
+        "使用文言文重寫輸入的字句，必須在140字元以內!"
+    ]
+    return random.choice(prompts)
 
-  # Assuming list_of_strings is a list containing strings from which you want to select
-  list_of_prompts = ["使用台灣作家瓊瑤的寫作風格重寫輸入的字句，必須在140字元以內!", "使用作家金庸的寫作風格重寫輸入的字句，必須在140字元以內!","使用文言文重寫輸入的字句，必須在140字元以內!"]
-  selected_string = random.choice(list_of_prompts)
-  return selected_string
+def main():
+    twitter_api_key = os.environ['TWITTER_API_KEY']
+    twitter_api_secret = os.environ['TWITTER_API_SECRET']
+    twitter_access_token = os.environ['TWITTER_ACCESS_TOKEN']
+    twitter_access_secret = os.environ['TWITTER_ACCESS_SECRET']
 
-selected_string = randomize_prompt()
+    twitter_client = TwitterClient(twitter_api_key, twitter_api_secret, twitter_access_token, twitter_access_secret)
 
-url = 'https://www.info.gov.hk/gia/rss/general_zh.xml'
-feed = feedparser.parse(url)
-first_summary_text = None
+    selected_prompt = select_random_prompt()
 
+    rss_feed_url = 'https://www.info.gov.hk/gia/rss/general_zh.xml'
+    feed = feedparser.parse(rss_feed_url)
 
-if feed.entries:
-  first_summary_text = re.sub('<[^<]+?>', '',
-                              feed.entries[0].title)  # Remove HTML tags
+    if feed.entries:
+        first_entry_title = extract_text_without_html(feed.entries[0].title)
 
-my_secret = os.environ['OPENAI_API_KEY']
-if first_summary_text:
-  response = completion(model="gpt-3.5-turbo",
-                        messages=[{
-                            "content": selected_string,
-                            "role": "system"
-                        }, {
-                            "content": first_summary_text,
-                            "role": "user"
-                        }])
+        openai_api_key = os.environ['OPENAI_API_KEY']
+        response_text = generate_response(selected_prompt, first_entry_title)
 
-# Now, we get the content from the response output using response_output function
-response_text = response_output(response)  # This captures the return value from response_output
-# Finally, tweet the content if it exists
+        if response_text:
+            tweet_text = first_entry_title + "\n\n" + response_text + "\n\n" + "#中文 #文學"
+            twitter_client.tweet(tweet_text)
+            print(tweet_text)
 
-if response_text:
-  tweet_text = first_summary_text + "\n\n" + response_text + "\n\n" + "#中文 #文學"
-  tweet_content(tweet_text)
-  print(tweet_text)
+if __name__ == "__main__":
+  main()
